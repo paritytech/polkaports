acd61dfaa02b4c0f3ad10bc21888f8996ae33712
Ivan Gankevich <ivan.gankevich@parity.io>
2025-06-06 13:04:27 +0200

    CI

--- ci/rust.sh
+++ ci/rust.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-cargo fmt --all --check --quiet
+cargo +nightly fmt --all --check --quiet
 cargo clippy --workspace --all-targets --all-features --quiet -- -Dwarnings
 cargo test --workspace --all-features --quiet -- --nocapture
