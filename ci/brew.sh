37b42168d62d651b59133aa0002035667eca2078
Ivan Gankevich <ivan.gankevich@parity.io>
2025-06-06 15:00:45 +0200

    CI

--- ci/brew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-lld_prefix=/opt/homebrew/opt/lld@19
-llvm_prefix=/opt/homebrew/Cellar/llvm@19/19.1.7
-brew install lld@19 llvm@19 automake autoconf libtool
-export PATH="$lld_prefix"/bin:"$llvm_prefix"/bin:"$PATH"
-ls "$lld_prefix"/bin/* \
-	"$llvm_prefix"/bin/* \
-	/opt/homebrew/bin/*
