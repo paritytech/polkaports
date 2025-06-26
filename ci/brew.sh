4e2b4f65d30922005f5c55cee01962ca95f38691
Ivan Gankevich <ivan.gankevich@parity.io>
2025-06-26 10:31:34 +0200

    Use GNU make on MacOS.

--- ci/brew.sh
+++ ci/brew.sh
@@ -2,14 +2,15 @@
 set -ex
 
 # Install dependencies.
-brew install lld@19 llvm@19 automake autoconf libtool gnu-sed
+brew install lld@19 llvm@19 automake autoconf libtool gnu-sed make
 
 # Add new commands to PATH.
 prefix=/opt/homebrew
 for bin in \
 	"$prefix"/Cellar/llvm@19/19.1.7/bin \
 	"$prefix"/opt/lld@19/bin \
-	"$prefix"/opt/gnu-sed/libexec/gnubin; do
+	"$prefix"/opt/gnu-sed/libexec/gnubin \
+	"$prefix"/opt/make/libexec/gnubin; do
 	if ! test -e "$bin"; then
 		printf "Directory %s doesn't exist.\n" "$bin" >&2
 		exit 1
