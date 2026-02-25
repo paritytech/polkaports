4ba62f5d3e1a6536a957a2876b3abd3c4d662978
Ivan Gankevich <ivan.gankevich@parity.io>
2026-02-25 15:41:37 +0100

    TODO.

--- apps/c++/main.cc
+++ apps/c++/main.cc
@@ -1,7 +1,7 @@
 #include <string>
 
 int main() {
-    // NOTE: I/o via std::cout and the likes isn't supported yet.
+    // TODO @ivan Change to normal hello world program when PolkaVM is updated to 0.31.0.
     std::string tmp("Hello world");
     return tmp.size();
 }
