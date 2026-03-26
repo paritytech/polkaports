08c2aefbeeba3f648469bec1d3aae429edf7bdf6
Ivan Gankevich <ivan.gankevich@parity.io>
2026-03-26 10:12:53 +0100

    CI

--- crates/corevm-dist/env.sh
+++ crates/corevm-dist/env.sh
@@ -1,8 +1,5 @@
-corevm_home="${COREVM_HOME:-$HOME/.corevm}"
+export COREVM_HOME="${COREVM_HOME:-$HOME/.corevm}"
 case ":$PATH:" in
-*:"$corevm_home"/bin:*) ;;
-*)
-	export PATH="$corevm_home/bin${PATH:+:}$PATH"
-	;;
+*:"$COREVM_HOME"/bin:*) ;;
+*) export PATH="$COREVM_HOME/bin${PATH:+:}$PATH" ;;
 esac
-unset corevm_home
